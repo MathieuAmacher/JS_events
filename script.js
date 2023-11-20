bf6bc3fc1257420f5ea53c730bb6beba4d363568
mathieu.amacher <amacher_mathieu@hotmail.com>
2023-11-20 17:29:53 +0100

    exo 5 pas fini

---
+++ script.js
@@ -0,0 +1,106 @@
+// 1 - clique sur le footer, affiche ''clique dans la console''
+
+// var footerClick= document.getElementsByTagName("footer");
+// console.log(footerClick);
+
+// var footerClick1 = footerClick[0];
+
+// footerClick1.addEventListener('click', Text1);
+    
+//     function Text1() {
+//         console.log("clique dans la console");
+//     }
+
+
+// 1 bis - clic numéro X
+
+// var footerClick= document.getElementsByTagName("footer");
+// var footerClick1 = footerClick[0];
+// let compteur = 0;
+
+// footerClick1.addEventListener('click', Text1);
+    
+//     function Text1() {
+//         console.log("clique dans la console");
+
+//     if (footerClick1 === true);
+//             compteur += 1;
+
+//     console.log(`clic numéro ${compteur}`);
+
+//     }
+
+
+// *************************************************************************************************
+
+// 2 - class Id navbar perde sa classe "collapse" si on clique et retrouve sa class collapse si on reclique dessus
+
+// var modify1 = document.getElementById("navbarHeader");
+// var clickon = document.getElementsByClassName("navbar-toggler-icon")[0];
+
+//   clickon.addEventListener('click', Collapse);
+
+//     function Collapse (){
+//         if (modify1.classList.contains("collapse"))
+//         {
+//             modify1.classList.remove("collapse");
+//         }
+//         else 
+//         {
+//         modify1.classList.add("collapse");
+//         }
+//     }
+
+// console.log(clickon);
+
+// ******************************************************************************************************
+
+
+// 3 - si on clique sur "edit" de la 1ere card, le texte de la card se met en rouge
+
+// var button = document.getElementsByClassName("btn-outline-secondary")[0];
+// var speech = document.getElementsByClassName("card-text")[0];
+
+// console.log(speech);
+
+//     button.addEventListener('click', red);
+
+//     function red () {
+//        speech.style.color = "#FF0000";
+//     }
+
+
+
+
+// *****************************************************************************************************
+
+// 4 - clique sur le Edit de la 2e card = vert, puis re-clique = noir
+
+// var button = document.getElementsByClassName("btn-outline-secondary")[1];
+// var speech = document.getElementsByClassName("card-text")[1];
+
+
+// console.log(speech);
+
+//         button.addEventListener('click', () => {
+//             speech.classList.toggle('green')
+//         });
+
+    
+    
+    
+// **************************************************************************************************
+
+// 5 - double click pour désactiver le CDN boostrap et encore double click pour remettre(pas fini)
+
+// var lien = document.getElementsByTagName("link")[0];
+// var navbar = document.getElementsByClassName("navbar")[0];
+// console.log(lien);
+
+// navbar.addEventListener('dblclick', supprimer);
+
+//     function supprimer () {
+//         lien.href="";
+//     }
+
+// ********************************************************************************************
